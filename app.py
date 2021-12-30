1c4e6e3b1bb79cb13557cf2eda3148918efcab58
Alexander Osetrov <aosetrov@dataart.com>
2021-12-30 23:43:58 +0300

    update flask port

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ init_db(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
--- static/js/collections/todos.js
+++ static/js/collections/todos.js
@@ -15,7 +15,7 @@ var app = app || {};
 
 		// Save all of the todo items under this example's namespace.
 		// localStorage: new Backbone.LocalStorage('todos-backbone'),
-		url: 'https:///todo-list-app-snchr.herokuapp.com:5000/items',
+		url: 'http://todo-list-app-snchr.herokuapp.com/items',
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
