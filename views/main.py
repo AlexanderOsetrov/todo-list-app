d37b2e2e75b47ec90d9d598602dfe1be24c5d0b3
Alexander Osetrov <aosetrov@dataart.com>
2022-09-16 20:00:54 +0400

    add settings page draft

--- views/main.py
+++ views/main.py
@@ -1,6 +1,6 @@
 from models.user import UserModel
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from app_setup import verify_authentication
 
 
@@ -21,3 +21,13 @@ def todos():
     user = UserModel.find_by_id(get_jwt_identity())
     todo_list = user.json()['items']
     return render_template('todos.html', todos=todo_list, user_id=user_id)
+
+
+@main.route('/settings')
+@jwt_required()
+def settings():
+    verify_authentication()
+    user = UserModel.find_by_id(get_jwt_identity())
+    if user.name != 'admin':
+        return redirect(url_for("main.index"))
+    return render_template('settings.html')
